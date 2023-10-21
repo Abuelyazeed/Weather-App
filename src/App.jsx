5322b42481a3c8952fffefebfda75312e15abbfb
Abuelyazeed <abuelyazeed175@gmail.com>
2023-10-21 03:15:04 +0300

    Added check weather based on current location feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,91 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { WiDaySunnyOvercast } from "react-icons/wi";
 import SearchCity from "./components/SearchCity";
 import WeatherTemperature from "./components/WeatherTemperature";
 import WeatherDetails from "./components/WeatherDetails";
+import fakeapi from "./data.json";
 
 function App() {
-  const [count, setCount] = useState(0);
   const apiKey = "d403a3476ecdac8bed27848b413064e9";
 
+  const [weatherData, setWeatherData] = useState({});
+
+  //The ipData is to display the name of the city based on the ip address
+  const [ipData, setIpData] = useState({});
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
+
+  //checks whether you have searched by the city or not to display the weather data
+  const [searchByCity, setSearchByCity] = useState(false);
+
+  //checks if we have reached the api limit
+  const [apiOverload, setApiOverload] = useState(false);
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  useEffect(() => {
+    if (lat && long) {
+      searchCityByIp();
+    }
+  }, [lat, long]);
+
+  async function getLocation() {
+    // const url = "https://ipapi.co/json";
+    // const response = await fetch(url);
+    // if (response.ok == false) {
+    //   setApiOverload(true);
+    // } else {
+    //   const data = await response.json();
+    //   setIpData(data);
+    //   setLat(data.latitude);
+    //   setLong(data.longitude);
+    // }
+    setIpData(fakeapi);
+    setLat(fakeapi.latitude);
+    setLong(fakeapi.longitude);
+    setApiOverload(true);
+  }
+
+  // GET LOCATION USING IP ADDRESS
+  async function searchCityByIp() {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=Metric&appid=${apiKey}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    setWeatherData(data);
+  }
+
+  // GET LOCATION USING CITY NAME
   async function searchCity(city) {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=${apiKey}`;
 
     const response = await fetch(url);
-    console.log(response);
+    const data = await response.json();
+
+    setWeatherData(data);
+    setSearchByCity(true);
+    setApiOverload(false);
   }
 
   return (
     <>
       <SearchCity searchCity={searchCity} />
-      <WeatherTemperature />
-      <WeatherDetails />
+
+      {apiOverload ? (
+        <p>Sorry, too many API requests at the moment</p>
+      ) : (
+        weatherData.main && (
+          <>
+            <WeatherTemperature
+              city={searchByCity ? weatherData.name : ipData.city}
+              temp={Math.round(weatherData.main.temp)}
+            />
+            <WeatherDetails />
+          </>
+        )
+      )}
     </>
   );
 }
--- src/components/WeatherTemperature.jsx
+++ src/components/WeatherTemperature.jsx
@@ -8,14 +8,12 @@ import rain_icon from "../assets/rain.png";
 import snow_icon from "../assets/snow.png";
 import wind_icon from "../assets/wind.png";
 
-function WeatherTemperature() {
-  const [temp, setTemp] = useState("");
-  const [city, setCity] = useState("");
+function WeatherTemperature({ city, temp }) {
   return (
     <div className="container">
       <img src={cloud_icon} className="weather-icon"></img>
-      <h1 className="temp">25&deg;C</h1>
-      <h1 className="city">London</h1>
+      <h1 className="temp">{temp}&deg;C</h1>
+      <h1 className="city">{city}</h1>
     </div>
   );
 }
