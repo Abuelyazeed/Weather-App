a821218bdb127fa908740f8b645c04241ca1ef93
Abuelyazeed <abuelyazeed175@gmail.com>
2023-10-21 17:54:19 +0300

    fixed some design issues and refactored code to be cleaner

--- src/App.jsx
+++ src/App.jsx
@@ -3,95 +3,70 @@ import "./App.css";
 import SearchCity from "./components/SearchCity";
 import WeatherTemperature from "./components/WeatherTemperature";
 import WeatherDetails from "./components/WeatherDetails";
-import fakeapi from "./data.json";
+
+const apiKey = "d403a3476ecdac8bed27848b413064e9";
 
 function App() {
-  const apiKey = "d403a3476ecdac8bed27848b413064e9";
-
   const [weatherData, setWeatherData] = useState({});
-
-  //The ipData is to display the name of the city based on the ip address
   const [ipData, setIpData] = useState({});
-  const [lat, setLat] = useState("");
-  const [long, setLong] = useState("");
-
-  //checks whether you have searched by the city or not to display the weather data
   const [searchByCity, setSearchByCity] = useState(false);
-
-  //checks if we have reached the api limit
-  const [apiOverload, setApiOverload] = useState(false);
 
   useEffect(() => {
     getLocation();
   }, []);
 
-  useEffect(() => {
-    if (lat && long) {
-      searchCityByIp();
-    }
-  }, [lat, long]);
-
   async function getLocation() {
-    // const url = "http://ip-api.com/json";
-    // const response = await fetch(url);
-    // if (response.ok == false) {
-    //   setApiOverload(true);
-    // } else {
-    //   const data = await response.json();
-    //   setIpData(data);
-    //   setLat(data.lat);
-    //   setLong(data.lon);
-    // }
-    setIpData(fakeapi);
-    setLat(fakeapi.latitude);
-    setLong(fakeapi.longitude);
-    setApiOverload(true);
+    try {
+      const ipApiUrl = "http://ip-api.com/json";
+      const response = await fetch(ipApiUrl);
+      const data = await response.json();
+      setIpData(data);
+      searchCityByCoordinates(data.lat, data.lon);
+    } catch (error) {
+      console.error("Error fetching location:", error);
+    }
   }
 
-  // GET LOCATION USING IP ADDRESS
-  async function searchCityByIp() {
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=Metric&appid=${apiKey}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    setWeatherData(data);
+  async function searchCityByCoordinates(lat, lon) {
+    try {
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=Metric&appid=${apiKey}`;
+      const response = await fetch(weatherUrl);
+      const data = await response.json();
+      setWeatherData(data);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
   }
 
-  // GET LOCATION USING CITY NAME
   async function searchCity(city) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=${apiKey}`;
-
-    const response = await fetch(url);
-    const data = await response.json();
-
-    setWeatherData(data);
-    setSearchByCity(true);
-    setApiOverload(false);
+    try {
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=${apiKey}`;
+      const response = await fetch(weatherUrl);
+      const data = await response.json();
+      setWeatherData(data);
+      setSearchByCity(true);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
   }
 
   return (
     <>
       <SearchCity searchCity={searchCity} />
       <div className="app-box">
-        {apiOverload ? (
-          <div>
-            <p>Unfortunatly we weren't able to detect your location😔</p>
-            <p>Please enter a city name to check its weather</p>
-          </div>
-        ) : (
-          weatherData.main && (
-            <>
-              <WeatherTemperature
-                city={searchByCity ? weatherData.name : ipData.city}
-                temp={Math.round(weatherData.main.temp)}
-                condition={weatherData.weather[0].main}
-              />
-              <WeatherDetails
-                feelsLike={Math.round(weatherData.main.feels_like)}
-                humidity={weatherData.main.humidity}
-                windSpeed={Math.round(weatherData.wind.speed)}
-              />
-            </>
-          )
+        {weatherData.main && (
+          <>
+            <WeatherTemperature
+              city={searchByCity ? weatherData.name : ipData.city}
+              temp={Math.round(weatherData.main.temp)}
+              condition={weatherData.weather[0].main}
+            />
+            <WeatherDetails
+              feelsLike={Math.round(weatherData.main.feels_like)}
+              humidity={weatherData.main.humidity}
+              windSpeed={Math.round(weatherData.wind.speed)}
+            />
+          </>
         )}
       </div>
     </>
--- src/components/WeatherTemperature.jsx
+++ src/components/WeatherTemperature.jsx
@@ -4,7 +4,6 @@ import "./WeatherTemperature.css";
 function WeatherTemperature({ city, temp, condition }) {
   return (
     <div className="box">
-      {/* <img src={icon_url} url className="weather-icon" alt="Weather Icon" /> */}
       <div className="city-temp">
         <h2 className="city">{city}</h2>
         <h1 className="temp">{temp}&deg;C</h1>
